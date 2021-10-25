8764d97cb697447b4d9164f0a1b42707d0cac007
Maxim Matous <maximmatous@seznam.cz>
2021-10-25 13:30:50 +0200

    Přidejte soubory projektu.

---
+++ nejvetsi_cislo.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main() {
+	int a = 8, b = 6, c = 2;
+	int max;
+
+	if (a == b) {
+		max = a;
+	}
+	else{
+		if ((a > b) && (a > c)) {
+			max = a;
+		}
+		else {
+			if ((b > a) && (b > c)) {
+				max = b;
+			}
+			else {
+				max = c;
+			}
+		}
+	}
+	printf("nejvetsi je cislo %d", max);
+}
